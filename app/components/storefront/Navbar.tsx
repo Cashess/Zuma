ac5da6cf1544da0a70618a9471532076b3c553c7
Aminu sulaiman <aminugotie42@gmail.com>
2024-10-15 18:10:57 +0000

    navbar

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -53,7 +53,7 @@ export async function Navbar() {
             />
           </>
         ) : (
-          <div className="hidden md:flex md:flex-1 md:items-center md:justify-end md:space-x-2">
+          <div className=" md:flex md:flex-1 md:items-center md:justify-end md:space-x-2">
             <Button variant="ghost" asChild>
               <LoginLink>Sign in</LoginLink>
             </Button>
